ca4c1c2dce1dc61ef23a8fd1eb19731dd0d2ebea
Jeongmin Woo <greenblues1190@gmail.com>
2022-04-09 17:42:00 +0900

    chore: cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const cors = require("json-server-cors");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
